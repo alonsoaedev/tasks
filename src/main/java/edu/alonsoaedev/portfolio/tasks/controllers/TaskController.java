614780660d08aeaf5c9e1ef3a9f1ffbb933101dd
Alonso Acosta-Enríquez <alonso.ae@hotmail.com>
2021-12-02 13:23:55 -0600

    Add the endpoint to retrieve all the tasks

--- src/main/java/edu/alonsoaedev/portfolio/tasks/controllers/TaskController.java
+++ src/main/java/edu/alonsoaedev/portfolio/tasks/controllers/TaskController.java
@@ -1,11 +1,16 @@
 package edu.alonsoaedev.portfolio.tasks.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 
 import edu.alonsoaedev.portfolio.tasks.services.TaskService;
+import edu.alonsoaedev.portfolio.tasks.entities.Task;
 
 @RestController
 @RequestMapping("api/v1/tasks")
@@ -16,5 +21,10 @@ public class TaskController {
     @Autowired
     public TaskController(@Qualifier("default") TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @GetMapping
+    ResponseEntity<List<Task>> listTasks() {
+        return ResponseEntity.ok(taskService.list());
     }
 }
