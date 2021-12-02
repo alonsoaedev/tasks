6b66ad6c978721b988da578bc257b2df94d8ee5c
Alonso Acosta-Enríquez <alonso.ae@hotmail.com>
2021-12-02 12:33:51 -0600

    Add the @Repository annotation

--- src/main/java/edu/alonsoaedev/portfolio/tasks/repositories/TaskRepository.java
+++ src/main/java/edu/alonsoaedev/portfolio/tasks/repositories/TaskRepository.java
@@ -1,8 +1,10 @@
 package edu.alonsoaedev.portfolio.tasks.repositories;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.alonsoaedev.portfolio.tasks.entities.Task;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
