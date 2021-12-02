ae62518516d58ede3f5903cb786c989020df19c7
Alonso Acosta-Enríquez <alonso.ae@hotmail.com>
2021-12-02 12:45:49 -0600

    Create a Task service implementation

---
+++ src/main/java/edu/alonsoaedev/portfolio/tasks/services/TaskServiceImp.java
@@ -0,0 +1,51 @@
+package edu.alonsoaedev.portfolio.tasks.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.alonsoaedev.portfolio.tasks.entities.Task;
+import edu.alonsoaedev.portfolio.tasks.repositories.TaskRepository;
+
+@Service
+@Qualifier("default")
+public class TaskServiceImp implements TaskService{
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public TaskServiceImp(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public List<Task> list() {
+        return null;
+    }
+
+    @Override
+    public Task add(String content) {
+        return null;
+    }
+
+    @Override
+    public Task retrieve(Long id) {
+        return null;
+    }
+
+    @Override
+    public Task modify(Long id, String content) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+
+    @Override
+    public Task check(Long id) {
+        return null;
+    }
+}
