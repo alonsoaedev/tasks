e9c0967eb9c16d95605677881b80f84d847b589c
Alonso Acosta-Enríquez <alonso.ae@hotmail.com>
2021-12-03 19:40:19 -0600

    Add the endpoint to add a new task

--- src/main/java/edu/alonsoaedev/portfolio/tasks/controllers/TaskController.java
+++ src/main/java/edu/alonsoaedev/portfolio/tasks/controllers/TaskController.java
@@ -7,10 +7,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import edu.alonsoaedev.portfolio.tasks.services.TaskService;
 import edu.alonsoaedev.portfolio.tasks.entities.Task;
+import edu.alonsoaedev.portfolio.tasks.exceptions.DuplicateTaskException;
+import edu.alonsoaedev.portfolio.tasks.exceptions.EmptyTaskContentException;
 
 @RestController
 @RequestMapping("api/v1/tasks")
@@ -26,5 +32,16 @@ public class TaskController {
     @GetMapping
     ResponseEntity<List<Task>> listTasks() {
         return ResponseEntity.ok(taskService.list());
+    }
+
+    @PostMapping
+    ResponseEntity<Task> addTask(@RequestBody Task newTask) {
+        try {
+            return ResponseEntity.status(201).body(taskService.add(newTask.getContent()));
+        } catch (DuplicateTaskException duplicateTaskException) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, duplicateTaskException.getMessage(), duplicateTaskException);
+        } catch (EmptyTaskContentException emptyTaskContentException) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, emptyTaskContentException.getMessage(), emptyTaskContentException);
+        }
     }
 }
