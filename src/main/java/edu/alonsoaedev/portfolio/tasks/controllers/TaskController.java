6d351f0f11ec8bf711d1ccf2ef36c3d4ea2dbbaf
Alonso Acosta-Enríquez <alonso.ae@hotmail.com>
2021-12-04 12:16:53 -0600

    Add the endpoint to delete a task

--- src/main/java/edu/alonsoaedev/portfolio/tasks/controllers/TaskController.java
+++ src/main/java/edu/alonsoaedev/portfolio/tasks/controllers/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
@@ -68,5 +69,11 @@ public class TaskController {
         } catch (TaskNotFoundException taskNotFoundException) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, taskNotFoundException.getMessage(), taskNotFoundException);
         }
+    }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<?> deleteTask(@PathVariable Long id) {
+        taskService.remove(id);
+        return ResponseEntity.noContent().build();
     }
 }
